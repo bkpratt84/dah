d27cc1f443531bb6c47baedaf4468e15c181e059
B <bkpratt84@gmail.com>
2018-01-11 19:49:39 -0600

    Initial Commit

---
+++ assets/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app dark>
+    <v-toolbar fixed app clipped-left class="elevation-0" color="primary">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+
+    <v-footer fixed app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import Meta from 'mixins/meta'
+
+  export default {
+    mixins: [Meta],
+
+    data () {
+      return {
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        title: 'Developers Against Humanity'
+      }
+    }
+  }
+</script>
---
+++ pages/WelcomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-text>
+          <p>Welcome.</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" flat router to="/game">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
